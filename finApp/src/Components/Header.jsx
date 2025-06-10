a24f0e9fa23fdf3fec71a6e616adc684965234f4
CodeSandbox <support@codesandbox.io>
2025-06-10 10:18:11 +0000

    Sandbox snapshot commit

---
+++ finApp/src/Components/Header.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+const getData = () => {
+  const date = new Date();
+  return date.getHours();
+};
+
+const Header = () => {
+  const [GreetingTxt, setGreetingTxt] = useState("Good Morning, user!");
+  const [userName, setUserName] = useState("Noah");
+
+  useEffect(() => {
+    if (getData() >= 12) {
+      setGreetingTxt(`Good Afternoon,${userName}`);
+    } else if (getData() >= 16 && getData <= 2) {
+      setGreetingTxt(`Good Evening, ${userName}`);
+    } else {
+      setGreetingTxt(`Good Morning, ${userName}`);
+    }
+  }, []);
+
+  return (
+    <header>
+      <h1>{GreetingTxt}</h1>
+      <div>
+        <button>Profile</button>
+        <button>Stats</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
