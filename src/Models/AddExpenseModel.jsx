882fc89f2b1391169aead6d9870e1ae6b8e50bea
Saravanan <94352332+Saravanan-e-2003@users.noreply.github.com>
2025-06-17 13:15:52 +0000

    Model updated

--- src/Models/AddExpenseModel.jsx
+++ src/Models/AddExpenseModel.jsx
@@ -8,11 +8,16 @@ export default function Modal({ isOpen, onClose }) {
       {" "}
       <div className="bg-white p-6 rounded shadow-lg w-96">
         <h2 className="text-xl font-bold mb-4">Add Expense</h2>
+        <label htmlFor="remark">Remark</label>
+        <input type="text" name="remark" id="remark-inp" placeholder="eg;-food,transport" />
+        <br />
+        <label htmlFor="amount">Amount</label>
+        <input type="text" name="amount" id="amount-inp" placeholder="10" />
         <button
           onClick={onClose}
           className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-400"
         >
-          Close{" "}
+          Cancel{" "}
         </button>{" "}
       </div>{" "}
     </div>
