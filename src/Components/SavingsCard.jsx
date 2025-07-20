7b7976fd6faa558dca893ce3896d8c9c7fb3957e
Saravanan <94352332+Saravanan-e-2003@users.noreply.github.com>
2025-07-20 12:31:53 +0000

    same res

--- src/Components/SavingsCard.jsx
+++ src/Components/SavingsCard.jsx
@@ -6,7 +6,7 @@ export default function SavingsCard(props){
     // const[isUpdateModel,setUpdateModel] = useState(false);
 
     return(
-        <div className='border shadow-[1px_2px_0_#1f1a14] rounded w-[calc(100%-40px)] md:w-[calc(50%-40px)] h-1/4'>
+        <div className='border shadow-[1px_2px_0_#1f1a14] rounded w-[calc(100%-40px)] md:w-[calc(50%-40px)] h-1/4 mx-auto'>
             <h2>{props.name}</h2>
             <p>{props.amount}</p>
             <button onClick={()=>{props.onRemove()}}>Remove</button>
